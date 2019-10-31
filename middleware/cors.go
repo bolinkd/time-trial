a199c027b85d26e6b6ff99c1f7bfe105bf867225
Dustin Bolink <williambolink@gmail.com>
2019-10-31 15:52:22 -0700

    Fixed CORS

--- middleware/cors.go
+++ middleware/cors.go
@@ -1,11 +1,21 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
+func GetEnv(key string, defaultVal string) string {
+	val := os.Getenv(key)
+	if len(val) == 0 {
+		return defaultVal
+	}
+	return val
+}
+
 func CORSMiddleware(c *gin.Context) {
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
+	c.Writer.Header().Set("Access-Control-Allow-Origin", GetEnv("ALLOW_ORIGIN", "http://localhost:4200"))
 	c.Writer.Header().Set("Access-Control-Max-Age", "86400")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Organization")
